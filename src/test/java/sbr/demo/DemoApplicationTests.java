fb0f6e82e24f1c087a877c8e6a268b0c8617380f
KrieqqeR <burakcevizli@gmail.com>
2024-04-21 17:31:07 +0300

    First Inıt for crud operations react with java

---
+++ src/test/java/sbr/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package sbr.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
